98da2a0a8c972a3e5a3f80fcf9a94def8fa48271
Arafa <javaintake@gmail.com>
2019-02-24 18:00:47 +0200

    Update Create Propertise File

--- DataSourceEmployee/src/datasourceemployee/CreateFile.java
+++ DataSourceEmployee/src/datasourceemployee/CreateFile.java
@@ -1,14 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package datasourceemployee;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Properties;
 
 /**
  *
  * @author PC
  */
-public class CreateFile {
-    
+public class CreateFile 
+{
+    public static void main(String[] args) throws IOException {
+        Properties properties = new Properties();
+        OutputStream outputStream = null;
+        try {
+            outputStream = new FileOutputStream("db.properties");
+            
+            properties.setProperty("MYSQL_DB_URL", "jdbc:mysql://localhost:3306/schema_arafa");
+            properties.setProperty("MYSQL_DB_USERNAME", "root");
+            properties.setProperty("MYSQL_DB_PASSWORD", "root");
+            
+            properties.store(outputStream, null);
+        } catch (IOException io) {
+            io.printStackTrace();
+        } finally {
+            if (outputStream != null) {
+                try {
+                    outputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
+
+
