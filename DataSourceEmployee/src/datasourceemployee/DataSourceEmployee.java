8ae8c9d459062181ab5ad55212cdf54c077f11fd
Nouran Amr <nouranamr206@gmail.com>
2019-02-24 18:51:05 +0200

    Launch Main Form

--- DataSourceEmployee/src/datasourceemployee/DataSourceEmployee.java
+++ DataSourceEmployee/src/datasourceemployee/DataSourceEmployee.java
@@ -21,22 +21,12 @@ public class DataSourceEmployee extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+       
+        FXMLBase root = new FXMLBase();
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        Scene scene = new Scene(root, 700, 500);
         
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("Employee Form");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
