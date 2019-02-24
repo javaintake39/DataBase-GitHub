73a08c69381e14f787718936702db54acba76fe0
Nouran Amr <nouranamr206@gmail.com>
2019-02-24 18:26:38 +0200

    Read DB properties from file

--- DataSourceEmployee/src/datasourceemployee/MyDataSourceFactory.java
+++ DataSourceEmployee/src/datasourceemployee/MyDataSourceFactory.java
@@ -1,5 +1,11 @@
 package datasourceemployee;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import javax.sql.DataSource;
+import com.mysql.cj.jdbc.MysqlDataSource;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,5 +17,21 @@ package datasourceemployee;
  * @author PC
  */
 public class MyDataSourceFactory {
-    
+    public static DataSource getMySQLDataSource() throws IOException {
+        Properties props = new Properties();
+        FileInputStream fis = null;
+        MysqlDataSource mysqlDS = null;
+        try {
+            fis = new FileInputStream("db.properties");
+            props.load(fis);
+            mysqlDS = new MysqlDataSource();
+            mysqlDS.setURL(props.getProperty("MYSQL_DB_URL"));
+            mysqlDS.setUser(props.getProperty("MYSQL_DB_USERNAME"));
+            mysqlDS.setPassword(props.getProperty("MYSQL_DB_PASSWORD"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return mysqlDS;
+    }
 }
